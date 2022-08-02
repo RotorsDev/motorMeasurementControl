ee9f51b7c8557ebe3fe00ce4ebe5d913873f125f
peter.gal <peter.gal@gmail.com>
2022-08-02 15:55:48 +0200

    Add project files.

---
+++ motorMeasurementControl/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace motorMeasurementControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.manual = new System.Windows.Forms.Button();
+            this.automatic = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(12, 393);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(437, 45);
+            this.trackBar1.SmallChange = 5;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(157, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 49);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(42, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PWM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(7, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(334, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 42);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "%";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(390, 362);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "100%";
+            // 
+            // manual
+            // 
+            this.manual.Enabled = false;
+            this.manual.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.manual.Location = new System.Drawing.Point(175, 90);
+            this.manual.Name = "manual";
+            this.manual.Size = new System.Drawing.Size(124, 49);
+            this.manual.TabIndex = 6;
+            this.manual.Text = "Manual";
+            this.manual.UseVisualStyleBackColor = true;
+            this.manual.Click += new System.EventHandler(this.manual_Click);
+            // 
+            // automatic
+            // 
+            this.automatic.Enabled = false;
+            this.automatic.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.automatic.Location = new System.Drawing.Point(104, 158);
+            this.automatic.Name = "automatic";
+            this.automatic.Size = new System.Drawing.Size(259, 43);
+            this.automatic.TabIndex = 7;
+            this.automatic.Text = "Automatic (linear)";
+            this.automatic.UseVisualStyleBackColor = true;
+            this.automatic.Click += new System.EventHandler(this.automatic_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // connect
+            // 
+            this.connect.Enabled = false;
+            this.connect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.connect.Location = new System.Drawing.Point(195, 3);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(156, 49);
+            this.connect.TabIndex = 9;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.connect);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(437, 55);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(3, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Choose a serial port";
+            // 
+            // start
+            // 
+            this.start.Enabled = false;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.start.Location = new System.Drawing.Point(12, 253);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(123, 37);
+            this.start.TabIndex = 11;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Enabled = false;
+            this.stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stop.Location = new System.Drawing.Point(352, 253);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(109, 37);
+            this.stop.TabIndex = 12;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(157, 208);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(5, 208);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Set increment delay";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(157, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Write value";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(324, 211);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "(default: 500 ms)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.automatic);
+            this.Controls.Add(this.manual);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button manual;
+        private System.Windows.Forms.Button automatic;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ motorMeasurementControl/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using CsvHelper;
+
+namespace motorMeasurementControl
+{
+    public partial class Form1 : Form
+    {
+        byte[] data = new byte[1];
+
+        string com;
+        SerialPort serial;
+        int delay = 100;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            com = comboBox1.SelectedItem.ToString();
+            serial = new SerialPort(com, 115200);
+            connect.Enabled = true;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            textBox1.Text = trackBar1.Value.ToString();
+
+            byte[] data = new byte[1];
+
+            //byte[] Numbers = BitConverter.GetBytes(trackBar1.Value);
+
+            byte pwm = Convert.ToByte(trackBar1.Value);
+
+            data[0] = pwm;
+
+            serial.Write(data, 0, 1);
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connect.Text == "Connect")
+                {
+                    serial.Open();
+                    connect.Text = "Disconnect";
+                    
+                }
+                else //button3.text == "Disconnect";
+                {
+                    trackBar1.Value = 0;
+                    trackBar1.Enabled = false;
+                    manual.Enabled = false;
+                    automatic.Enabled = false;
+                    start.Enabled = false;
+                    stop.Enabled = false;
+                    manual.BackColor = Control.DefaultBackColor;
+                    automatic.BackColor = Control.DefaultBackColor;
+                    serial.Close();
+                    
+                    connect.Text = "Connect";
+                }
+
+                manual.Enabled = true;
+                automatic.Enabled = true;
+            }
+            catch 
+            {
+                MessageBox.Show("No serial port detected / Serial port not available.");
+            }
+        }
+
+        private void manual_Click(object sender, EventArgs e)
+        {
+            automatic.Enabled = false;
+            start.Enabled = true;
+            stop.Enabled = true;
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            manual.BackColor = Color.Green;
+        }
+
+        async void start_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+
+            start.Enabled = false ;
+
+            serial.DataReceived += new SerialDataReceivedEventHandler(serial_DataReceived);
+
+            if (manual.BackColor == Color.Green)
+            {
+                trackBar1.Enabled = true;
+
+            }
+            else if (automatic.BackColor == Color.Green)
+            {
+                
+
+                for (int i = 0; i < 100; i++)
+                {
+                    if (automatic.BackColor == Color.Green)
+                    {
+                        if (textBox2.Text.Length > 0)
+                        {
+                            delay = int.Parse(textBox2.Text);
+                        }
+
+                        textBox1.Text = i.ToString();
+                        byte[] data = new byte[1];
+                        data[0] = Convert.ToByte(i);
+                        serial.Write(data, 0, 1);
+
+                        await Task.Delay(delay);
+                    }
+                }
+
+                for (int j = 100; j >= 0; j--)
+                {
+                    if (automatic.BackColor == Color.Green)
+                    {
+                        if (textBox2.Text.Length > 0)
+                        {
+                            delay = int.Parse(textBox2.Text);
+                        }
+                        textBox1.Text = j.ToString();
+                        byte[] data = new byte[1]; 
+                        data[0] = Convert.ToByte(j);
+                        serial.Write(data, 0, 1);
+
+                        await Task.Delay(delay);
+                    }
+                }
+
+                automatic.BackColor = Control.DefaultBackColor;
+            } 
+        }
+
+        private void automatic_Click(object sender, EventArgs e)
+        {
+            automatic.BackColor = Color.Green;
+            manual.Enabled = false;
+            start.Enabled = true;
+            stop.Enabled = true;
+            textBox2.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1];
+            data[0] = Convert.ToByte(0);
+            serial.Write(data, 0, 1);
+
+            serial.DataReceived -= serial_DataReceived;
+
+            trackBar1.Value = 0;
+            textBox1.Text = trackBar1.Value.ToString();
+
+            trackBar1.Enabled = false;
+            start.Enabled = false;
+            button1.Enabled = false;
+
+            automatic.BackColor = Control.DefaultBackColor;
+            manual.BackColor = Control.DefaultBackColor;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value = int.Parse(textBox1.Text);
+
+            if (trackBar1.Enabled == true)
+            {
+                byte[] data = new byte[1];
+                data[0] = Convert.ToByte(int.Parse(textBox1.Text));
+                serial.Write(data, 0, 1);
+            }
+            else
+            {
+                byte[] data = new byte[1];
+                data[0] = Convert.ToByte(int.Parse(textBox1.Text));
+                serial.Write(data, 0, 1);
+            }
+            
+        }
+
+        void serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // this the read buffer
+            byte[] buff = new byte[30000];
+            int readByteCount = serial.BaseStream.Read(buff, 0, serial.BytesToRead);
+            // you can specify other encodings, or use default
+            string response = System.Text.Encoding.UTF8.GetString(buff);
+
+            // you need to implement AppendToFile ;)
+            //
+            AppendToFile(String.Format("response :{0}", response));
+
+            // Or, just send sp.ReadExisting();
+            if (serial.ReadExisting() != null)
+            {
+                AppendToFile(serial.ReadExisting());
+            }
+        }
+
+        private void AppendToFile(string toAppend)
+        {
+            string myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string myFile = "\\motor.txt";
+            File.AppendAllText(myDocumentsPath+myFile, toAppend + Environment.NewLine);
+        }
+
+       /* private void LogTxt()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filename = "\\motor.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path + filename))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path+filename))
+                {
+                    sw.Write("Hello ");
+                    sw.Write("And ");
+                    sw.WriteLine("Welcome");
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path+filename))
+            {
+                serial.ReadExisting();
+            }
+        }*/
+    }
+}
